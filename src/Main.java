2fe10d8b031d553bed6bac101fe293b5ca32be3f
AllenChen-12 <allenc2028@student.cis.edu.hk>
2025-09-18 08:18:16 +0800

    methods

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,38 @@ public class Main {
     }
 
     // 1. add
-    
+    public static int add(int a, int b) {
+        return a + b;
+    }
     // 2. add
-
+public static int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
     // 3. morningGreeting
+public static String morningGreeting(String name) {
+        return "早上好, " + name + "!";
+    }
 
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String name) {
+        return "下午好, " + name + "!";
+    }
     // 5. triple
+    public static int triple(int x) {
+        return x + x + x;
+    }
 
     // 6. half
-
+public static double half(double x) {
+        return x / 2;
+    }
     // 7. roundPositiveValueToNearestInteger
-
+public static int roundPositiveValueToNearestInteger(double x) {
+        return (int) Math.round(x);
+    }
     // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger(double x) {
+        return (int) Math.round(x - 0.01);
+    }
 
 }
